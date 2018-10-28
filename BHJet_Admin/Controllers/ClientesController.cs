38544684a2aeb40393f1b4d7a5eac51c2b392d78
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2018-10-28 20:04:42 -0300

    -

---
+++ BHJet_Admin/Controllers/ClientesController.cs
@@ -0,0 +1,79 @@
+﻿using BHJet_Admin.Models;
+using BHJet_Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHJet_Admin.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Clientes()
+        {
+            return View(new ClientesModel()
+            {
+                ListModel = new List<ClienteModel>()
+                 {
+                    new ClienteModel()
+                    {
+                        ClienteID = 1,
+                        NomeRazaoSocial = "Tânia Azevedo Araujo",
+                        TipoContrato = TipoContrato.ChamadosAvulsos
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 2,
+                        NomeRazaoSocial = "André Rodrigues Oliveira",
+                        TipoContrato = TipoContrato.ChamadosAvulsos
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 3,
+                        NomeRazaoSocial = "Kauã Souza Azevedo",
+                        TipoContrato = TipoContrato.ChamadosAvulsos
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 4,
+                        NomeRazaoSocial = "Melissa Castro Ribeiro",
+                        TipoContrato = TipoContrato.ContratoLocacao
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 5,
+                        NomeRazaoSocial = "Caio Cunha Fernandes",
+                        TipoContrato = TipoContrato.ContratoLocacao
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 6,
+                        NomeRazaoSocial = "Emilly Costa Rodrigues",
+                        TipoContrato = TipoContrato.ChamadosAvulsos
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 7,
+                        NomeRazaoSocial = "Maria Pinto Lima",
+                        TipoContrato = TipoContrato.ContratoLocacao
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 8,
+                        NomeRazaoSocial = "Alex Melo Pereira",
+                        TipoContrato = TipoContrato.ContratoLocacao
+                    },
+                    new ClienteModel()
+                    {
+                        ClienteID = 9,
+                        NomeRazaoSocial = "Sarah Dias Martins",
+                        TipoContrato = TipoContrato.ChamadosAvulsos
+                    }
+                 }
+            });
+
+        }
+    }
+}
---
+++ BHJet_Core/Enum/TipoContrato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHJet_Core.Enum
+{
+    public enum TipoContrato
+    {
+        ChamadosAvulsos = 1,
+        ContratoLocacao = 2
+    }
+}
