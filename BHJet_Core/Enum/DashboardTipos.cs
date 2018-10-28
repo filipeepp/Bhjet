b2de294b60d6f15bcc49c9dc4ebf4044a02de1f4
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2018-10-28 14:20:17 -0300

    teste commit - DIOGO

--- BHJet_Core/Enum/DashboardTipos.cs
+++ BHJet_Core/Enum/DashboardTipos.cs
@@ -5,6 +5,7 @@
         ChamadoAguardandoMotociclista = 1,
         ChamadoAguardandoCarros = 2,
         MotociclistaDisponivel = 3,
-        CarrosDisponiveis = 4
+        CarrosDisponiveis = 4,
+        teste = 5
     }
 }
