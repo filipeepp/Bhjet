41439339da81ccb7d2f3b9567cff4a3ed8c2d24b
LEOZI <LEOZI@DESKTOP-LLPUVTI>
2018-10-30 00:27:30 -0300

    -

---
+++ BHJet_Admin/Models/Dashboard/DiariaModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BHJet_Admin.Models.Dashboard
+{
+    public class DiariaModel
+    {
+        private DateTime _PeriodoInicial;
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime PeriodoInicial
+        {
+            get
+            {
+                return _PeriodoInicial;
+            }
+            set
+            {
+                _PeriodoInicial = value;
+            }
+        }
+
+    }
+}
