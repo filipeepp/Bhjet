9375c0217acd0dde6dbe7f7513c4effafe31fed0
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2018-11-18 18:45:14 -0200

    --

--- BHJet_Admin/Scripts/NovoCliente.js
+++ BHJet_Admin/Scripts/NovoCliente.js
@@ -14,36 +14,15 @@
     $('#DadosCadastrais_CPFCNPJ').mask(cpfMascara, cpfOptions);
 
         //CEP
-    var options = {
-        onComplete: function (cep) {
-            //Nova variável "cep" somente com dígitos.
-            var cep = $("#DadosCadastrais_CEP").val().replace(/\D/g, '');
-            //Verifica se campo cep possui valor informado.
-            if (cep != "") {
-                //Expressão regular para validar o CEP.
-                var validacep = /^[0-9]{8}$/;
-                //Valida o formato do CEP.
-                if (validacep.test(cep)) {
-                    //Consulta o webservice viacep.com.br/
-                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
-                        if (!("erro" in dados)) {
-                            //Atualiza os campos com os valores da consulta.
-                            $("#DadosCadastrais_Endereco").val(dados.logradouro);
-                            $("#DadosCadastrais_Bairro").val(dados.bairro);
-                            $("#DadosCadastrais_Cidade").val(dados.localidade);
-                            $("#DadosCadastrais_Estado").val(dados.uf);
-                            $("#DadosCadastrais_NumeroEndereco").focus();
-
-                        } //end if.
-                        else {
-                            limpaEndereco();
-                        }
-                    });
-                } //end if.
-            } //end if.
-        },
-    };
-    $('#DadosCadastrais_CEP').mask('00000-000', options);
+    var endereco = {
+        Cep : $("#DadosCadastrais_CEP"),
+        Rua : $("#DadosCadastrais_Endereco"),
+        Bairro : $("#DadosCadastrais_Bairro"),
+        Cidade : $("#DadosCadastrais_Cidade"),
+        Estado : $("#DadosCadastrais_Estado"),
+        NumeroEndereco : $("#DadosCadastrais_NumeroEndereco")
+    }
+    $('#DadosCadastrais_CEP').mask('00000-000', GetOptionsViaCep(endereco));
 
     /*CONTATO*/
         //TELEFONE COMERCIAL
