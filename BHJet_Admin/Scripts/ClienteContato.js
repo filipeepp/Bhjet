ea500db85c3ba9dabb4da6c6d49790393aab5c5b
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2018-11-29 03:23:16 -0200

    --

--- BHJet_Admin/Scripts/ClienteContato.js
+++ BHJet_Admin/Scripts/ClienteContato.js
@@ -1,21 +1,35 @@
 ﻿$(document).ready(function () {
 
+
+
 	var aberturaSpan = '<span class="text-danger field-validation-error" data-valmsg-replace="true"><span>';
 	var fechamentoSpan = '</span></span></span>';
 
 	var reEmail = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+	var reTelefoneComercial = /^(\([0-9]{2}\))\s([9]{1})?([0-9]{4})-([0-9]{4})$/;
+	var reTelefoneCelular = /^(\([0-9]{2}\))\s([9]{1})?([0-9]{4})-([0-9]{4})$/;
 
-	//Limpa Erro quando digitado
-	/*var reEmail = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
-		if (!$(this).val()) 
-			!reEmail.test($(this).val()) ? $("#hdfErrorEmail").html(aberturaSpan + "E-mail inválido" + fechamentoSpan) : "";*/
+
+	//Limpa erro do campo data quando alterado
+	$("input[name$='DataNascimento']").change(function () {
+
+		$("div[id$='DataNascimento']").each(function () {
+			var aux = $(this).children('input');
+			if (aux[0].value)
+				$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
+		});
+	
+	});
+
+
+	//Limpa Erro quando digitado e trata expressões regulares
 	$("input[name^='Contato']").keyup(function () {
 
 		$("div[id^='Contato']").each(function () {
 			var aux = $(this).children('input');
-			$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
-
+		
 			if (aux[0].value) {
+				$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
 
 				if (aux.hasClass("ctmErrorEmail")) {
 
@@ -24,14 +38,28 @@
 					if (!reEmail.test(aux[0].value))
 						$(this).children('span[id$="' + aux[0].id + '"]').html(aberturaSpan + 'E-mail inválido.' + fechamentoSpan);
 					else
-						$(this).children('span[id$="' + aux[0].id + '"]').html("");
+						$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
 
-				} else {
+				} else if (aux.hasClass("ctmErrorTelefoneComercial")) {
 
-					$(this).children('span[id$="' + aux[0].id + '"]').html("");
+					$(this).append('<span id="spanError_' + aux[0].id + '" >');
+
+					if (!reTelefoneComercial.test(aux[0].value))
+						$(this).children('span[id$="' + aux[0].id + '"]').html(aberturaSpan + 'Telefone Comercial inválido.' + fechamentoSpan);
+					else
+						$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
+
+				} else if (aux.hasClass("ctmErrorTelefoneCelular")) {
+
+					$(this).append('<span id="spanError_' + aux[0].id + '" >');
+
+					if (!reTelefoneCelular.test(aux[0].value))
+						$(this).children('span[id$="' + aux[0].id + '"]').html(aberturaSpan + 'Telefone Celular inválido.' + fechamentoSpan);
+					else
+						$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
+
 				}
 
-				//$(this).children('span[id$="' + aux[0].id + '"]').html("");
 			}
 
 		});
@@ -61,6 +89,10 @@ window.ValidarContato = function () {
 			aux.hasClass("ctmErrorDataNascimento") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + "A Data de Nascimento é obrigatória" + fechamentoSpan) : "";
 
 		}
+
+		//Desabilita botão de avançar caso encontre erro
+		if ($(document).find('span[id^="spanError_"]').length > 0)
+			$("#lnkValidarContato").addClass("isDisabled");
 	});
 
 };
