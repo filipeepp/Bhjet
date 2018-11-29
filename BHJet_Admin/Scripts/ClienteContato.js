4de3b1b8e09994c021a65108306fde9f226c07cc
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2018-11-29 01:16:35 -0200

    --

---
+++ BHJet_Admin/Scripts/ClienteContato.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+
+	var aberturaSpan = '<span class="text-danger field-validation-error" data-valmsg-replace="true"><span>';
+	var fechamentoSpan = '</span></span></span>';
+
+	var reEmail = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+
+	//Limpa Erro quando digitado
+	/*var reEmail = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+		if (!$(this).val()) 
+			!reEmail.test($(this).val()) ? $("#hdfErrorEmail").html(aberturaSpan + "E-mail inválido" + fechamentoSpan) : "";*/
+	$("input[name^='Contato']").keyup(function () {
+
+		$("div[id^='Contato']").each(function () {
+			var aux = $(this).children('input');
+			$(this).children('span[id="spanError_' + aux[0].id + '"]').remove();
+
+			if (aux[0].value) {
+
+				if (aux.hasClass("ctmErrorEmail")) {
+
+					$(this).append('<span id="spanError_' + aux[0].id + '" >');
+
+					if (!reEmail.test(aux[0].value))
+						$(this).children('span[id$="' + aux[0].id + '"]').html(aberturaSpan + 'E-mail inválido.' + fechamentoSpan);
+					else
+						$(this).children('span[id$="' + aux[0].id + '"]').html("");
+
+				} else {
+
+					$(this).children('span[id$="' + aux[0].id + '"]').html("");
+				}
+
+				//$(this).children('span[id$="' + aux[0].id + '"]').html("");
+			}
+
+		});
+	});
+
+});
+
+window.ValidarContato = function () {
+
+	var aberturaSpan = '<span class="text-danger field-validation-error" data-valmsg-replace="true"><span>';
+	var fechamentoSpan = '</span></span></span>';
+
+	$("div[id^='Contato']").each(function () {
+		var aux = $(this).children('input');
+		if (!aux[0].value) {
+			//Contato
+			aux.hasClass("ctmErrorContato") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + 'Nome do Contato obrigatório.' + fechamentoSpan) : "";
+			//Email
+			aux.hasClass("ctmErrorEmail") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + "E-mail obrigatório." + fechamentoSpan) : "";
+			//Telefone Comercial
+			aux.hasClass("ctmErrorTelefoneComercial") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + "Telefone Comercial obrigatório." + fechamentoSpan) : "";
+			//Telefone Celular
+			aux.hasClass("ctmErrorTelefoneCelular") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + "Telefone Celular obrigatório." + fechamentoSpan) : "";
+			//Setor
+			aux.hasClass("ctmErrorSetor") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + "Setor é obrigatório." + fechamentoSpan) : "";
+			//Data Nascimento
+			aux.hasClass("ctmErrorDataNascimento") ? $(this).append('<span id="spanError_' + aux[0].id + '" >' + aberturaSpan + "A Data de Nascimento é obrigatória" + fechamentoSpan) : "";
+
+		}
+	});
+
+};
