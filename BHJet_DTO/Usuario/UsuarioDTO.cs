4ecce32e9ef457fac2d0ec11a16a151f7378e72a
lhsilva <lhsilva@SRSBHW043>
2018-11-29 12:29:53 -0200

    -

---
+++ BHJet_DTO/Usuario/UsuarioDTO.cs
@@ -0,0 +1,14 @@
+﻿using BHJet_Core.Enum;
+
+namespace BHJet_DTO.Usuario
+{
+    public class UsuarioDTO
+    {
+        public long ID { get; set; }
+        public string Email { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
+        public bool Situacao { get; set; }
+        public string SituacaoDesc { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ BHJet_Servico/Usuario/UsuarioServico.cs
@@ -0,0 +1,41 @@
+﻿using BHJet_DTO.Usuario;
+using System;
+
+namespace BHJet_Servico.Usuario
+{
+    public interface IUsuarioServico
+    {
+        UsuarioDTO[] BuscaListaUsuarios(string trechoPesquisa);
+        void CadastrarUsuario(UsuarioDTO model);
+        void AtualizaSituacao(int situacao, long id);
+        void DeletaUsuario(long id);
+    }
+
+    public class UsuarioServico : ServicoBase, IUsuarioServico
+    {
+        public UsuarioServico(string token) : base(token)
+        {
+
+        }
+
+        public UsuarioDTO[] BuscaListaUsuarios(string trechoPesquisa)
+        {
+            return this.Get<UsuarioDTO[]>(new Uri($"{ServicoRotas.Base}{ServicoRotas.Usuario.GetUsuarios}?trecho={trechoPesquisa}"));
+        }
+
+        public void CadastrarUsuario(UsuarioDTO model)
+        {
+            this.Post(new Uri($"{ServicoRotas.Base}{ServicoRotas.Usuario.PostUsuario}"), model);
+        }
+
+        public void AtualizaSituacao(int situacao, long id)
+        {
+            this.Put(new Uri($"{ServicoRotas.Base}{string.Format(ServicoRotas.Usuario.PutSituacao, situacao, id)}"), "");
+        }
+
+        public void DeletaUsuario(long id)
+        {
+            this.Delete(new Uri($"{ServicoRotas.Base}{string.Format(ServicoRotas.Usuario.DeleteUsuario, id)}"));
+        }
+    }
+}
