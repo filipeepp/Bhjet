98204b97c81457fdca4eb9e7ae9dc1af883a5462
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2019-01-10 22:28:58 -0200

    --

--- BHJet_Admin/Scripts/NovoCliente.js
+++ BHJet_Admin/Scripts/NovoCliente.js
@@ -13,7 +13,16 @@
                 field.mask(cpfMascara.apply({}, arguments), options);
             }
         };
-    $('#DadosCadastrais_CPFCNPJ').mask(cpfMascara, cpfOptions);
+	$('#DadosCadastrais_CPFCNPJ').mask(cpfMascara, cpfOptions);
+
+	//Adiciona ou remove regra required do campo Inscrição Estadual a partir do que foi digitado no campo CPF/CNPJ
+	$('#DadosCadastrais_CPFCNPJ').on('change', function () {
+		
+		if ($('#DadosCadastrais_CPFCNPJ').val().replace(/\D/g, '').length > 11) 
+			$('#DadosCadastrais_InscricaoEstadual').rules("add", "required")
+		else 
+			$('#DadosCadastrais_InscricaoEstadual').rules("remove", "required")
+	});
 
         //CEP
     var endereco = {
