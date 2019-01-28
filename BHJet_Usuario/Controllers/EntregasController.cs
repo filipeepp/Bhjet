4945ef52d7117d4ce1d90b77d734a318a59c612f
Diogo Dorneles Nominato <nominato.diogo@gmail.com>
2019-01-27 22:30:05 -0200

    --

---
+++ BHJet_Usuario/Controllers/EntregasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHJet_Usuario.Controllers
+{
+    public class EntregasController : Controller
+    {
+        // GET: Entregas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
